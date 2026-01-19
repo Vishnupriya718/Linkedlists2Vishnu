b89840e27e35093d6e100f0c58c506f3f0f6dfe8
Vishnupriya Chunduri <vishnupriyachunduri@Vishnupriyas-MacBook-Air.local>
2026-01-18 16:39:00 -0800

    prtoypes for the function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
-int main() {
-    // your code here
-}
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include "node.h"
+#include "student.h"
+
+using namespace std;
+
+// yk prototypes so main knows these exist
+void addStudent(node*& head);
+void addRecursive(node*& current, node* newNode);
+
+void printStudents(node* head);
+void printRecursive(node* current);
+
+void deleteStudent(node*& head);
+void deleteRecursive(node*& current, int id);
+
+void averageGpa(node* head);
+void averageRecursive(node* current, float& sum, int& count);
+
  
